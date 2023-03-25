0a82bed1a2a347674087e1914aade29da3cd2c16
SARB <mrsinghbusiness05@gmail.com>
2023-03-25 05:46:17 +0530

    feat - GetAllPosts Done

--- Backend/Controllers/PostController.js
+++ Backend/Controllers/PostController.js
@@ -1,9 +1,9 @@
 const Post = require('../Models/Post');
 const  asyncHandler = require('express-async-handler');
-
 const JWT_SECRET = process.env.JWT_SECRET;
 
 const createpost = asyncHandler(async(req,res) => {
+
     try{
         const {title,body} = req.body;
         if(!title || !body){
@@ -29,4 +29,22 @@ const createpost = asyncHandler(async(req,res) => {
     }
 })
 
-module.exports = {  createpost };
+
+const allposts = asyncHandler(async(req,res) => {
+    try{
+        Post.find()
+        .populate("postedBy","_id name")
+        .then(posts => {
+            res.json({posts})
+        })
+        .catch(err => {
+            console.log(err)
+        })
+    }catch(error)
+    {
+        console.log(error);
+        res.status(422).json(' Something Worng ')
+    }
+})
+
+module.exports = {  createpost ,allposts };
--- Backend/Routes/Post.js
+++ Backend/Routes/Post.js
@@ -1,10 +1,12 @@
 
 const express = require('express');
 const router = express.Router();
-const { createpost } = require('../Controllers/PostController');
+const { createpost  ,allposts } = require('../Controllers/PostController');
 
 const protectedroute = require('../Middleware/RequireLogin');
 
-router.route('/createpost').post(protectedroute,createpost)
+router.route('/createpost').post(protectedroute,createpost);
+
+router.route('/allposts').get(protectedroute,allposts)
 
 module.exports = router;
